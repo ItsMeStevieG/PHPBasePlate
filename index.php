16872e02e0edb4c80754ffa146e3355f76b5f379
Stevie G <ItsMeStevieG@users.noreply.github.com>
2019-04-26 13:31:37 +1000

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
+require_once("config.php");
+
+$multiset=array(
+    "PageTitle"=>"Welcome to Steven-Graham.com",
+    "MetaDescription"=>"Steven-Graham.com - I.T. services, sales, support, remote assistance, web design, web hosting, troubleshooting, networking, custom built computers and more.",
+    "MetaKeywords"=>"Computer Repair, Computer Engineer, Web Design, Web Application Design, Web Developer, PHP, HTML, CSS",
+    "PageAuthor"=>"Steven Graham",
+    "BodyContent"=>$content
+);
+
+$template->multiSet($multiset);
+echo $template->output();
 
 ?>
